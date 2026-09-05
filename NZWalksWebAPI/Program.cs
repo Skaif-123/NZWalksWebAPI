843646e7e6205069e1b08850eb983539882ce2f9
KaifBhombal123 <kaifbhombal.kb@gmail.com>
2026-09-05 10:41:24 +0530

    Created DBCOntext and added DBContext DI code in Program.cs

--- NZWalksWebAPI/Program.cs
+++ NZWalksWebAPI/Program.cs
@@ -14,6 +14,19 @@ builder.Services.AddDbContext<NZWalksDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
 });
 
+
+// Add the SchoolCoursesDBContext to the service container
+// This allows the application to use the SchoolCoursesDBContext for database operations related to school courses.
+//We are basically adding or passing connection string to the SchoolCoursesDBContext 
+//whenever This context is called we can use the connection string to connect to the database and perform operations.
+
+builder.Services.AddDbContext<SchoolCoursesDBContext>(options => {
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolCourse"));
+});
+
+
+
 var app = builder.Build();
 
 
